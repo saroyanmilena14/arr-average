3f668f860b691906ab6a1be5a6b51396eccdf670
Milena <64065041+saroyanmilena14@users.noreply.github.com>
2020-07-23 12:39:18 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function average (numbers) {
     let sum = 0;
-    for (let i = 0; i <= numbers.length; i++){
-        sum += i;
+    for (let i = 0; i < numbers.length; i++){
+        sum += numbers[i];
     }
     return sum / numbers.length;
   }
